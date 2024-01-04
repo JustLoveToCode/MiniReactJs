bbe2f8cbfe1e8a5286c4c6eaef159df53b67db49
JustLoveToCode <135109684+JustLoveToCode@users.noreply.github.com>
2024-01-04 13:45:36 +0800

    Add files via upload
    This is the First Commit for the Pizza Order

---
+++ Pizza Order ReactJs/src/index.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+// This is the Arrays that have the Objects that is inside the Array:
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+
+
+
+function App(){
+    return(
+    <div className='container'>
+{/* Displaying the Header, Menu and Footer Component */}
+        <Header/>
+        <Menu/>
+        <Footer/>
+    </div>
+    )
+}
+
+function Header(){
+    // style={color: 'red', fontSize: '32px', textTransform: 'uppercase'}
+    const style ={};
+ return(
+<header className="header">
+    <h1 style={style} className="header">Fast React Pizza Co.</h1>
+</header>
+ )
+}
+
+function Menu(){
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    // pizzas.length() is used to get the number of items in the Array:
+    const numPizzas = pizzas.length;
+
+    return(
+    <main className="menu">
+        <h2>Our Menu</h2>
+{/* Using the Tenary Operator */}
+        {numPizzas > 0 ? (
+        <>
+        <p>We are actually serving Authentic Dishes</p>
+        <ul className="pizzas">
+{/* Using the map Method to Loop through the Array */}
+        {pizzas.map((pizza)=>(
+        <Pizza pizzaObj = {pizza}
+        key={pizza.name}/>
+        ))}
+        </ul>
+        </>
+// If the conditions of numPizzas > 0 is not satisfied:
+        ) : <p>We are still working on our Menu, Please
+            Come Back Later</p>}
+
+    </main>
+    )
+}
+
+
+// Passing the props of pizzaObj as the Object:
+function Pizza({pizzaObj}){
+   
+
+    // if(pizzaObj.soldOut) return null;
+
+    return(
+        
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
+        <img src={pizzaObj.photoName} alt={pizzaObj.name}/>
+        <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+      
+        <span>{pizzaObj.soldOut?'SOLD OUT':pizzaObj.price}</span>
+        </div>
+    </li>
+    )
+}
+
+function Footer(){
+// Using the new Date() Method to get the Current Date:
+const hour = new Date().getHours();
+const openHour = 12;
+const closeHour = 22;
+const isOpen = hour >=openHour && hour <=closeHour;
+
+return(
+<footer className="footer">
+{/* If isOpen is True */}
+    {isOpen ? (
+    <Order
+    closeHour={closeHour}
+    openHour ={openHour}/>
+    ): <p>We are happy to welcome you between {openHour}:00 and {closeHour}:00</p>}
+</footer>
+)}
+
+function Order({closeHour, openHour}){
+    return(
+    <div className='order'>
+        <p>We are Open from {openHour}:00 Until {closeHour}:00,
+        Come Visit us or Order Online</p>
+        <button className='btn'>Order Now</button>
+    </div>
+    )
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App/>);
